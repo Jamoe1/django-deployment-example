94ddef469c5c667348f2c1e84c5081c3ebfdb592
James <james_oburu@hotmail.com>
2020-06-24 07:15:18 +0100

    first

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+from django.urls import include
+
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from basic_app import views
+from django.contrib import admin
+from django.urls import path
+from basic_app import views
+from django.urls import include
+
+#  TEMPLATE URLS!
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+
+]
